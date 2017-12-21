7f21166b4b47c518fc420c15ea7daa406f124092
Bea <bea.hernandez@datmean.com>
2017-12-21 23:11:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Polyanna R-Ladies
+
+2017 ha been an awesome year for R-Ladies. We have grown so much, meet so many new and 💜 people. We speak with some of this awesome people almost daily, and with the holiday spirit we decided to do a _Polyanna / Secret Santa / גמד וענק / Yankee swap / Amigo Invisible_.
+
+I hope this script will be helpful to anyone who need it but in reality it was [Sarah and Lucy](http://livefreeordichotomize.com/2017/11/15/secret-sampling/)'s work.
+
+## 2017
+
+The form to participate will close on December 21st and the next day emails will fly to their secret companion.
---
+++ script.R
@@ -0,0 +1,52 @@
+library(readr)
+library(dplyr)
+
+# Test file with same columns
+# file <- "MOCK_DATA.csv"
+# Automatic names are generated when making the spreadsheet like "Dirección de 
+# correo electrónico" which is basically "email" 🤷
+df <- read_csv(file) %>% 
+  rename(email = `Dirección de correo electrónico`) %>%
+  rename(name = Name) 
+
+set.seed(525)
+dat <- df %>%
+  select(name) %>%
+  mutate(name = sample(name)) %>%
+  mutate(partner = lag(name))
+dat$partner[1] <- dat$name[nrow(dat)]
+
+df_name <- df[ , c("name", "email")]
+df_partner <- df[ , c("name", "Interests", "Address")]
+dat <- dat %>%
+  left_join(df_name, c("name" = "name")) %>%
+  left_join(df_partner, c("partner" = "name"))
+
+### Send email
+library(ponyexpress)
+body <- "Dear {name},
+
+You have an important mission to complete, you have been assigned to surprise <b>{partner}</b> with a <3 gift. 
+Maybe you know each other, maybe you have talked some time, what we know is that {partner} told us she has the following interests:
+
+<b>{Interests}</b>
+
+We have set the limit to $15 or to a paperback book price, you know what we mean.
+
+<img src = 'https://media.giphy.com/media/zhPXoVIBMtnUs/giphy.gif'> </img>
+
+Also, don't forget to send your gift to
+
+<b>{Address}</b>
+
+Have the best of Christmas,
+R-Ladies"
+
+our_template <- glue::glue(glitter_template)
+
+parcel <- parcel_create(df, sender_name = "BR-Lady, sender_email = "temailgmail.com",
+                        subject = "Polyanna!", template = our_template)
+
+parcel_preview(parcel)     
+
+parcel_send(parcel)

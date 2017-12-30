28352ae6edbc69fc0615c0a29871d240e2a22d4b
Bea <bea.hernandez@datmean.com>
2017-12-30 16:48:29 +0100

    Last modifications to script

--- script.R
+++ script.R
@@ -3,13 +3,14 @@ library(dplyr)
 
 # Test file with same columns
 # file <- "MOCK_DATA.csv"
+
 # Automatic names are generated when making the spreadsheet like "Dirección de 
 # correo electrónico" which is basically "email" 🤷
-df <- read_csv(file) %>% 
+df <- read_csv("R-Ladies 2017 Polyanna (Respuestas) - Respuestas de formulario 1.csv") %>% 
   rename(email = `Dirección de correo electrónico`) %>%
   rename(name = Name) 
 
-set.seed(525)
+set.seed(525) # Set seed to reproduce matching in case not every mail gets to its destination
 dat <- df %>%
   select(name) %>%
   mutate(name = sample(name)) %>%
@@ -26,25 +27,24 @@ dat <- dat %>%
 library(ponyexpress)
 body <- "Dear {name},
 
-You have an important mission to complete, you have been assigned to surprise <b>{partner}</b> with a <3 gift. 
-Maybe you know each other, maybe you have talked some time, what we know is that {partner} told us she has the following interests:
-
-<b>{Interests}</b>
-
-We have set the limit to $15 or to a paperback book price, you know what we mean.
-
-<img src = 'https://media.giphy.com/media/zhPXoVIBMtnUs/giphy.gif'> </img>
-
-Also, don't forget to send your gift to
-
-<b>{Address}</b>
-
-Have the best of Christmas,
-R-Ladies"
+  You have an important mission to complete, you have been assigned to surprise <b>{partner}</b> with a <3 gift. 
+  Maybe you know each other, maybe you have talked some time, what we know is that {partner} told us she has the following interests:
+  
+  <b>{Interests}</b>
+  
+  We have set the limit to $15 or to a paperback book price, you know what we mean.
+  
+  <img src = 'https://media.giphy.com/media/zhPXoVIBMtnUs/giphy.gif'> </img>
+  
+  Also, don't forget to send your gift to
+  
+  <b>{Address}</b>
+  
+  Have the best of Christmas,
+  R-Ladies"
 
 our_template <- glue::glue(glitter_template)
-
-parcel <- parcel_create(dat, sender_name = "BR-Lady, sender_email = "temailgmail.com",
+parcel <- parcel_create(dat, sender_name = "Sender", sender_email = "email@gmail.com",
                         subject = "Polyanna!", template = our_template)
 
 parcel_preview(parcel)     
